d9496ccb487655f420c78b95f4081f96543ea079
茶子枫 <1511608962@qq.com>
2024-06-18 16:48:26 +0800

    登录实现

--- src/controller/userController.go
+++ src/controller/userController.go
@@ -5,6 +5,8 @@ import (
 	"gin_demo/src/entity"
 	"gin_demo/src/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 	"log"
 	"net/http"
@@ -45,10 +47,19 @@ func Register(context *gin.Context) {
 		return
 	}
 	//创建用户
+	//密码加密
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"code": 500,
+			"msg":  "加密错误",
+		})
+		return
+	}
 	user := entity.User{
 		Name:      name,
 		Telephone: telephone,
-		Password:  password,
+		Password:  string(hashedPassword),
 	}
 	db.Create(&user)
 	//返回结果
@@ -57,7 +68,62 @@ func Register(context *gin.Context) {
 	})
 }
 
-// 判断数据库中是否存在该手机号
+func Login(context *gin.Context) {
+	db := common.GetDB()
+	//获取参数
+	telephone := context.PostForm("telephone")
+	password := context.PostForm("password")
+
+	//数据验证
+	if len(telephone) != 11 {
+		context.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "手机号必须为11位",
+		})
+		return
+	}
+	if len(password) < 6 {
+		context.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "密码不得小于6位",
+		})
+		return
+	}
+
+	//判断手机号是否存在
+	var user entity.User
+	db.Where("telephone = ?", telephone).First(&user)
+	if user.ID == 0 {
+		context.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "用户不存在",
+		})
+		return
+	}
+
+	//判断密码是否正确
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"code": 400,
+			"msg":  "密码错误",
+		})
+		return
+	}
+
+	//发放token
+	token := uuid.New()
+
+	context.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"msg":  "登陆成功",
+		"data": gin.H{
+			"token": token,
+		},
+	})
+}
+
+// 判断数据库中不存在该手机号
 func isTelephoneExist(db *gorm.DB, telephone string) bool {
 	var user entity.User
 	db.Where("telephone = ?", telephone).First(&user)
--- src/utils/utils.go
+++ src/utils/utils.go
@@ -7,4 +7,5 @@ const NAME_PREFIX = "user_"
 // RandomString 生成随机用户名
 func RandomString() string {
 	return (NAME_PREFIX + uuid.New().String())[:10]
+
 }
